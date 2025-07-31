1afac82e3d71a04ff6fd46000a77a7b8fe2df88c
PerisicJere <jereperisic@icloud.com>
2025-07-30 22:23:41 -0400

    Delete volmir directory

--- volmir/api/auth/login/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from api.auth.login import login_view
-
-urlpatterns = [
-    path('', login_view.login, name='login'),
-]
--- volmir/api/auth/signup/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from api.auth.signup import signup_view
-
-urlpatterns = [
-    path('', signup_view.signup, name='signup'),
-]
